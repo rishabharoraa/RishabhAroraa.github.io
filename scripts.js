ef1b55029e3a8afecabe244cb1cb7c9c07aea0cf
Rishabh Arora <rishabharora.ns@gmail.com>
2021-06-17 21:00:02 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,13 @@
+let i = 0;
+let text = ' & a Full Stack Web Developer'; /* The text */
+let speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < text.length) {
+    document.getElementById("typewriter").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+typeWriter();
